98156c4c3160bb42529d70af48478bda6754fdee
theoldworldcitadel <theoldworldcitadel@gmail.com>
2026-05-28 16:05:37 -0600

    Polished Event Configuration + Final Updates

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-25613826'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.c73doqc04n8"
+    "revision": "0.fj5udurhuo4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
